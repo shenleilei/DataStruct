ce0b842acd36a8eb930698134bebf7afb40875d0
shenleilei <shenshenlei@hotmail.com>
2015-10-06 11:15:19 +0800

    不带头结点的对头结点插入删除比较麻烦

--- summary/不带头结点链表API.c
+++ summary/不带头结点链表API.c
@@ -1,3 +1,4 @@
+//不带头节点的明显操作比较简单
 #pragma warning(disable:4996)
 #include<stdio.h>
 #include <stdlib.h>
@@ -6,66 +7,41 @@ typedef struct LinkNode {
 	ElemType data;
 	struct LinkNode *next;
 }LinkNode, *LinkList;
-//链表初始化
-void InitLinkedList(LinkList* list) {
-	*list = (LinkNode*)malloc(sizeof(struct LinkNode));
-	(*list)->next = NULL;
-}
+
 //链表在最后位置插入
 void Insert(LinkList *list, ElemType elem) {
 	LinkList l = (*list);
-	while (l->next != NULL) l = l->next;
-	LinkNode* node = (LinkNode*)malloc(sizeof(LinkNode));
-	node->data = elem;
-	node->next = NULL;
-	l->next = node;
-}
-//在链表的指定位置插入，i=1表示在第一个位置插入该值
-InsertLinkListAtPos(LinkList *list, int i, ElemType elem) {
-	LinkList l = *list;
-	while (--i)  l = l->next;
-	LinkNode* node = (LinkNode*)malloc(sizeof(LinkNode));
-	node->data = elem;
-	node->next = NULL;
-	node->next = l->next;
-	l->next = node;
+	while (l->next!= NULL) l = l->next;
+	l->next= (LinkNode*)malloc(sizeof(LinkNode));
+	l->next->data = elem;
+	l->next->next = NULL;
 }
 
 //删除链表最后位置的元素
 void DeleteLastElem(LinkList *list) {
 	LinkList l = (*list);
-	while (l->next->next->next != NULL) l = l->next;
-	l->next->next = l->next->next->next;
+	while (l->next->next!= NULL) l = l->next;
+	l->next=NULL;
 }
 
-//删除指定位置i的链表值，并用elem返回
-DeleteLinkeListAtPos(LinkList *list, int i, ElemType* elem) {
-	LinkList l = *list;
-	while (--i) l = l->next;
-	LinkList p = l->next;
-	*elem = p->data;
-	l->next = l->next->next;
-	free(p);
-}
 //链表的创建
 void CreateLinkList(LinkList *list) {
 	int data;
 	scanf("%d", &data);
 	if (data != 0)
 	{
-		LinkNode* node = (LinkNode*)malloc(sizeof(LinkNode));
-		node->data = data;
-		node->next = NULL;
-		(*list)->next = node;
+		(*list) = (LinkNode*)malloc(sizeof(LinkNode));
+		(*list)->data = data;
+		(*list)->next = NULL;
 		CreateLinkList(&(*list)->next);
 	}
 }
 //遍历链表
-void Traversal(LinkList list){
-	while (list->next)
+void Traversal(LinkList list) {
+	while (list)
 	{
-		printf("%d ", list->next->data);
-		list = list->next;	
+		printf("%d ", list->data);
+		list = list->next;
 	}
 }
 //逆序打印链表
@@ -73,22 +49,13 @@ void InvertedLinkList(LinkList list) {
 	if (list->next) {
 		InvertedLinkList(list->next);
 	}
-	printf("%d\n",list->data);
+	printf("%d  ", list->data);
 }
 int main() {
-	LinkList list;
-	//ElemType elem;
-	InitLinkedList(&list);
+	LinkList list=NULL;
 	CreateLinkList(&list);
+	Insert(&list, 5);
 	DeleteLastElem(&list);
-
-	//InsertLinkListAtPos(&list, 1, 3);
-	//DeleteLinkeListAtPos(&list,2,&elem );
-	//InvertedLinkList(list);
-	//printf("%d", elem);
 	Traversal(list);
-	
-	//Insert(&list, 5);
-	//printf("%d\n",list->next->data);
 	return 0;
 }
